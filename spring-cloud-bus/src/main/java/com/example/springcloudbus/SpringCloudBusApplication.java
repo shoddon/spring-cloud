dbb8090a28b6654c06253868c29f3051fb2031a4
shoddon <shoddon@163.com>
2018-11-06 10:30:40 +0800

    加入config配置文件及config-client

--- spring-cloud-bus/src/main/java/com/example/springcloudbus/SpringCloudBusApplication.java
+++ spring-cloud-bus/src/main/java/com/example/springcloudbus/SpringCloudBusApplication.java
@@ -2,10 +2,10 @@ package com.example.springcloudbus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableConfigServer
 public class SpringCloudBusApplication {
 
 	public static void main(String[] args) {
