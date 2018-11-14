6046ca68ab8a071f73eb964153b80a501a2dccba
shoddon <shoddon@163.com>
2018-11-14 16:14:22 +0800

    去掉配置文件加密

--- root/com.shoddon.encryption/EncryptionPropertyConfig/EncryptionPropertyConfig.java
+++ root/com.shoddon.encryption/EncryptionPropertyConfig/EncryptionPropertyConfig.java
@@ -1,44 +1,44 @@
-import com.ulisesbocchio.jasyptspringboot.EncryptablePropertyResolver;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.util.UUID;
-
-/**
- * @类功能说明：配置文件加解密bean
- * @类修改者：
- * @修改日期：
- * @修改说明：
- * @作者： shoddon
- * @创建时间： 2018/11/6 10:41
- * @版本：V1.0
- */
-@Configuration
-public class EncryptionPropertyConfig {
-    @Bean(name="encryptablePropertyResolver")
-    public EncryptablePropertyResolver encryptablePropertyResolver() {
-        return new EncryptionPropertyResolver();
-    }
-
-    class EncryptionPropertyResolver implements EncryptablePropertyResolver {
-
-        @Override
-        public String resolvePropertyValue(String value) {
-            if(StringUtils.isBlank(value)) {
-                return value;
-            }
-            return resolveDESValue(value);
-        }
-
-        private String resolveDESValue(String value) {
-            // 自定义DES密文解密
-            try {
-                return DESUtil.decryption(value, UUID.randomUUID().toString());
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-    }
-}
+//import com.ulisesbocchio.jasyptspringboot.EncryptablePropertyResolver;
+//import org.apache.commons.lang3.StringUtils;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import java.util.UUID;
+//
+///**
+// * @类功能说明：配置文件加解密bean
+// * @类修改者：
+// * @修改日期：
+// * @修改说明：
+// * @作者： shoddon
+// * @创建时间： 2018/11/6 10:41
+// * @版本：V1.0
+// */
+//@Configuration
+//public class EncryptionPropertyConfig {
+//    @Bean(name="encryptablePropertyResolver")
+//    public EncryptablePropertyResolver encryptablePropertyResolver() {
+//        return new EncryptionPropertyResolver();
+//    }
+//
+//    class EncryptionPropertyResolver implements EncryptablePropertyResolver {
+//
+//        @Override
+//        public String resolvePropertyValue(String value) {
+//            if(StringUtils.isBlank(value)) {
+//                return value;
+//            }
+//            return resolveDESValue(value);
+//        }
+//
+//        private String resolveDESValue(String value) {
+//            // 自定义DES密文解密
+//            try {
+//                return DESUtil.decryption(value, UUID.randomUUID().toString());
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//            return null;
+//        }
+//    }
+//}
--- spring-cloud-config-client/src/main/java/com/example/springcloudconfigclient/SpringCloudConfigClientApplication.java
+++ spring-cloud-config-client/src/main/java/com/example/springcloudconfigclient/SpringCloudConfigClientApplication.java
@@ -1,12 +1,26 @@
 package com.example.springcloudconfigclient;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@RestController
 public class SpringCloudConfigClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudConfigClientApplication.class, args);
+	}
+
+	@Value("${myww}") // git配置文件里的key
+	String myww;
+
+	@RequestMapping(value = "/hi")
+	public String hi(){
+		return myww;
 	}
 }
